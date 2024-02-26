091b50fba5a911687be8347af2a25dd91dc8cb32
Bronze2 <leedongsu97@naver.com>
2024-02-26 18:38:43 +0900

    test

---
+++ DirectX3D/Manager/ColliderManager.h
@@ -0,0 +1,70 @@
+#pragma once
+class ColliderManager : public Singleton<ColliderManager>
+{
+private:
+    friend class Singleton;
+
+   
+
+    ColliderManager();
+    ~ColliderManager();
+
+public:
+    void SetPlayer(Player* player) { this->player = player; playerCollider = player->GetCollider(); }
+
+    enum Collision_Type {
+        PLAYER,
+        ORC,
+        WALL,
+        ARROW,
+        END,
+
+
+    };
+
+    void SetPlayer(Player* player, CapsuleCollider* playerCollider) { this->player = player; this->playerCollider = playerCollider; }
+    void SetObstacles(Collider* obstacle) { obstacles.push_back(obstacle); }
+
+    void PushPlayer();
+    void SetHeight();
+
+    void GuiRender();
+
+    float CloseRayCollisionColliderDistance(Ray ray);
+
+    ColliderModel* CreateColliderModel(string mName, string mTag, Vector3 mScale, Vector3 mRot, Vector3 mPos);
+
+
+
+    bool ControlPlayer(Vector3* dir);
+    bool SetPlayerHeight(Collider* obstacle);
+
+
+    void PushCollision(Collision_Type Type, Collider* _pCollider) { vecCol[Type].push_back(_pCollider); }
+
+    vector<Collider*>& Getvector(Collision_Type _type) { return vecCol[_type]; }
+
+
+    bool CollisionCheck(Collider* _pCollider, Collision_Type _vecCol);
+    bool CollisionCheck(Collision_Type _type1,Collision_Type _type2);
+private:
+    Player* player;
+    CapsuleCollider* playerCollider;
+    vector<Collider*> obstacles;
+
+    Collider* onBlock;  //현재 밑에있는 콜라이더 Contact로 대체 가능할듯
+
+
+    vector<Collider*> vecCol[Collision_Type::END];
+
+
+    Ray* playerFoot;
+
+    float maxHeight;
+
+    Vector3 rayHeight;
+
+    Ray* footRay;
+    Ray* headRay;
+};
+
