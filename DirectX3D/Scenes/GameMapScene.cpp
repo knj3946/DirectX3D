8f96a867dcd0d432daf48b31c3de740e1a1cff7d
nackjoon kim <skrwns304@naver.com>
2024-03-21 14:25:01 +0900

    프레임 제한

--- DirectX3D/Scenes/GameMapScene.cpp
+++ DirectX3D/Scenes/GameMapScene.cpp
@@ -53,6 +53,8 @@ void GameMapScene::Update()
 
 	if (MenuManager::Get()->GetSelectGameMenu() == 1) //인게임
 	{
+		Timer::Get()->SaveStartTime();
+
 		if (MenuManager::Get()->GetSelectFailMenu() == 1)
 		{
 			//처음부터 시작하도록 로직작성
@@ -576,5 +578,6 @@ void GameMapScene::FirstLoading()
 	if (MenuManager::Get()->GetLoadingSequence() == 6)
 	{
 		MenuManager::Get()->SetLoadingRate(100.f);
+		Timer::Get()->SetStartFlag(true);
 	}
 }
